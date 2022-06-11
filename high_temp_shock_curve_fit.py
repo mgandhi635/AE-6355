f898e184c954c37ced7b3794d32d7a8da1c71cb2
Manan Gandhi <mgandhi@gatech.edu>
2022-06-11 16:21:50 -0400

    Finish 2c

--- high_temp_shock_curve_fit.py
+++ high_temp_shock_curve_fit.py
@@ -1,17 +1,19 @@
 import numpy as np
 import pandas as pd
 
-data = pd.read_excel('Curve_fit_constants_for_class_r2.xlsx', skiprows=1)
+gamma_data = pd.read_excel('Curve_fit_constants_for_class_r2.xlsx', skiprows=1, sheet_name='h=h(p,rho)')
+temp_data = pd.read_excel('Curve_fit_constants_for_class_r2.xlsx', skiprows=1, sheet_name='T=T(P,rho)')
 
 # xl = pd.ExcelFile('Curve_fit_constants_for_class_r2.xlsx')
 # df = xl.parse(skiprows=1)
 
-print(data)
+# print(gamma_data)
+# print(temp_data)
 
 
 class GammaTildeCurveFit:
     def __init__(self):
-        self.data = data
+        self.data = gamma_data
 
     def fit(self, p_2_est, rho_2_est):
         (X, Y, Z) = self.get_vars(p_2_est, rho_2_est)
@@ -22,12 +24,12 @@ class GammaTildeCurveFit:
     @staticmethod
     def evaluate_curve(X, Y, Z, coeffs):
         num = coeffs[4] + coeffs[5]*Y + coeffs[6]*Z + coeffs[7]*Y*Z
-        den = 1 + np.exp(coeffs[8]*(X + coeffs[9]*Y + coeffs[11]))
+        den = 1 + np.exp(coeffs[8]*(X + coeffs[9]*Y + coeffs[10]))
         return coeffs[0] + coeffs[1]*Y + coeffs[2]*Z + coeffs[3]*Y*Z + num/den
 
     def get_coefficents(self, Y, Z):
         row = self.get_coeff_row(Y, Z)
-        return data.iloc[row, 2:]
+        return self.data.iloc[row, 2:-1]
 
     def get_vars(self, p_2_est, rho_2_est):
         X = self.compute_X(p_2_est)
@@ -101,11 +103,102 @@ class GammaTildeCurveFit:
         return X - Y
 
 
+class HighTempCurveFit:
+    def __init__(self):
+        self.data = temp_data
+
+    def fit(self, p_2_est, rho_2_est):
+        (X, Y, Z) = self.get_vars(p_2_est, rho_2_est)
+        coeffs, T0 = self.get_coefficents(Y, Z)
+        curve_out = self.evaluate_curve(X, Y, Z, coeffs)
+        temp_ratio = 10 ** (curve_out)
+        temp = temp_ratio * T0
+        return temp, (X, Y, Z), coeffs, T0
+
+    @staticmethod
+    def evaluate_curve(X, Y, Z, coeffs):
+        num = coeffs[5] + coeffs[6]*Y + coeffs[7]*Z + coeffs[8]*Y*Z + coeffs[9]*Z*Z
+        den = 1 + np.exp(coeffs[10]*(Z + coeffs[11]))
+        return coeffs[0] + coeffs[1]*Y + coeffs[2]*Z + coeffs[3]*Y*Z + coeffs[4]*Z*Z + num/den
+
+    def get_coefficents(self, Y, Z):
+        row, T0 = self.get_coeff_row(Y, Z)
+        return self.data.iloc[row, 2:-1], T0
+
+    def get_vars(self, p_2_est, rho_2_est):
+        X = self.compute_X(p_2_est)
+        Y = self.compute_Y(rho_2_est)
+        Z = self.compute_Z(X, Y)
+        return (X, Y, Z)
+
+    def get_coeff_row(self, Y, Z):
+        T0_1 = 151.78  # Kelvin
+        T0_2 = 0.55556  # Kelvin
+        if Y > -0.50:
+            return self.find_z_range_1(Z), T0_1
+        elif (-4.5 < Y) and (Y <= -0.5):
+            return self.find_z_range_2(Z), T0_1
+        elif (-7 < Y) and (Y <= -4.5):
+            return self.find_z_range_3(Z), T0_2
+        else:
+            raise ValueError
+
+    @staticmethod
+    def find_z_range_1(Z):
+        if (0.48 < Z) and (Z <= 0.90):
+            return 0
+        elif Z > 0.9:
+            return 1
+        else:
+            raise ValueError
+
+    @staticmethod
+    def find_z_range_2(Z):
+        if (.48 < Z) and (Z <= 0.9165):
+            return 2
+        elif (0.9165 < Z) and (Z <= 1.478):
+            return 3
+        elif (1.478 < Z) and (Z <= 2.176):
+            return 4
+        elif Z > 2.176:
+            return 5
+        else:
+            raise ValueError
+
+    @staticmethod
+    def find_z_range_3(Z):
+        if (0.3 < Z) and (Z <= 1.07):
+            return 6
+        elif (1.07 < Z) and (Z <= 1.57):
+            return 7
+        elif (1.57 < Z) and (Z <= 2.24):
+            return 8
+        elif Z > 2.24:
+            return 9
+        else:
+            raise ValueError
+
+    @staticmethod
+    def compute_X(p_2_est):
+        return np.log10(p_2_est / 1.0134e5)
+
+    @staticmethod
+    def compute_Y(rho_2_est):
+        return np.log10(rho_2_est / 1.225)
+
+    @staticmethod
+    def compute_Z(X, Y):
+        return X - Y
+
+
 if __name__ == "__main__":
     p_2_est = 4004.7616416966903
     rho_2_est = 3.99109e-5  # kg / m^3
 
     gamma_curve = GammaTildeCurveFit()
+    temp_curve = HighTempCurveFit()
 
-    out = gamma_curve.fit(p_2_est, rho_2_est)
-    print(out)
+    gamma = gamma_curve.fit(p_2_est, rho_2_est)
+    temp = temp_curve.fit(p_2_est, rho_2_est)
+    print(gamma)
+    print(temp)
--- problem_1_2.py
+++ problem_1_2.py
@@ -1,7 +1,7 @@
 import numpy as np
 import scipy as sp
-# import jax.numpy as jnp
 import scipy.optimize
+from high_temp_shock_curve_fit import GammaTildeCurveFit, HighTempCurveFit
 
 gamma_air = 1.4  # gas constant air
 shock_angle = np.radians(90)
@@ -38,22 +38,27 @@ p_1 = 2.3875  # N / m^2
 # part a:
 print("\nPART A:")
 m_1_a = cpg_velocity_to_mach(u_1, T_1, gamma_air, molar_mass_air)
-print(f"Upstream Mach number: {m_1_a}")
+# print(f"Upstream Mach number: {m_1_a}")
 
-pressure_ratio_a = cpg_pressure_ratio(gamma_air, m_1_a, shock_angle)
+pressure_ratio_a = cpg_pressure_ratio(gamma_air, m_1_a, shock_angle)  # rho_2 / rho_1
 print(f"Pressure ratio across normal shock: {pressure_ratio_a}")
 
 density_ratio_a = cpg_density_ratio(gamma_air, m_1_a, shock_angle)
 print(f"Density ratio across normal shock: {density_ratio_a}")
 
+# print(f"Epsilon across normal shock: {1 / density_ratio_a}")
+
 temperature_ratio_a = cpg_temperature_ratio(gamma_air, m_1_a, shock_angle)
 print(f"Temperature ratio across normal shock: {temperature_ratio_a}")
-print(f"Temperature downstream of normal shock {temperature_ratio_a * T_1}")
+# print(f"Temperature downstream of normal shock {temperature_ratio_a * T_1}")
+
+
 # part b
 """
 Assume that air is approximated as pure diatomic nitrogen in local thermodynamic equilibrium
 Use the following function for enthalpy
 """
+print("\nPART B:")
 
 cp_N2_200K = 1.039  # specific heat of nitrogen gas at 200 Kelvin in kJ / (kg K)
 R_N2 = R_universal / molar_mass_N2
@@ -67,8 +72,7 @@ def enthalpy_N2(temp):  # Kelvin
 h_1 = cp_N2_200K * T_1
 kinetic_1 = u_1 ** 2 / 2
 
-density_ratio_b_est = 0.1
-print("\nPART B:")
+epsilon_b_est = 0.1
 
 # Iterative procedure
 # p_2_est = p_1 + rho_1 * u_1 ** 2 * (1 - density_ratio_b_est)
@@ -84,6 +88,7 @@ print("\nPART B:")
 # print(f"Enthalpy check downstream of normal shock {h_2_check}")
 # print(f"Enthalpy difference: {h_2_check - h_2_est:e}")
 
+
 def outer_loop(e0, p_1, rho_1, h_1, u_1, T_1):
     p_2_est = p_1 + rho_1 * u_1 ** 2 * (1 - e0)
     h_2_est = h_1 + u_1 ** 2 / 2 * (1 - e0 ** 2)
@@ -97,15 +102,16 @@ def outer_loop(e0, p_1, rho_1, h_1, u_1, T_1):
     eps_check = rho_1 / rho_2
     return e0 - eps_check
 
-density_ratio_b = sp.optimize.newton(outer_loop, density_ratio_b_est, args=(p_1, rho_1, h_1, u_1, T_1))
+epsilon_b = sp.optimize.newton(outer_loop, epsilon_b_est, args=(p_1, rho_1, h_1, u_1, T_1))
 
-p_2_b = p_1 + rho_1 * u_1 ** 2 * (1 - density_ratio_b)
-h_2_b = h_1 + u_1 ** 2 / 2 * (1 - density_ratio_b ** 2)
-rho_2_b = rho_1 / density_ratio_b
+p_2_b = p_1 + rho_1 * u_1 ** 2 * (1 - epsilon_b)
+h_2_b = h_1 + u_1 ** 2 / 2 * (1 - epsilon_b ** 2)
+rho_2_b = rho_1 / epsilon_b
 T_2_b = p_2_b / (rho_2_b * R_N2)
-print(f"Pressure downstream of normal shock {p_2_b}")
-print(f"Enthalpy downstream of normal shock {h_2_b}")
-print(f"Temperature downstream of normal shock {T_2_b}")
+# print(f"Pressure downstream of normal shock {p_2_b}")
+# print(f"Enthalpy downstream of normal shock {h_2_b}")
+# print(f"Temperature downstream of normal shock {T_2_b}")
+# print(f"Density downstream of normal shock {rho_2_b}")
 
 
 pressure_ratio_b = p_2_b / p_1
@@ -114,12 +120,76 @@ temperature_ratio_b = T_2_b / T_1
 print(f"Pressure ratio across normal shock: {pressure_ratio_b}")
 
 print(f"Density ratio across normal shock: {density_ratio_b}")
+# print(f"Epsilon across normal shock: {epsilon_b}")
 
 print(f"Temperature ratio across normal shock: {temperature_ratio_b}")
 
-# Guess a temperature
-# T_2_est = (T_1 * temperature_ratio_a)  #+ -34141.763
-# h_2_check = enthalpy_N2(T_2_est)
-# print(f"Temperature downstream of normal shock {T_2_est}")
-# print(f"Enthalpy check downstream of normal shock {h_2_check}")
-# print(f"Enthalpy difference: {h_2_check - h_2_est:e}")
+# part c:
+print("\nPART C:")
+
+#initialize the curve fits
+gamma_curve = GammaTildeCurveFit()
+temp_curve = HighTempCurveFit()
+
+
+def compute_number_density(p_2, T_2):
+    return (p_2 / 101325.) * (273.15 / T_2)  # Pa and Kelvin
+
+
+def compute_curve_fit_enthaply(p_2, rho_2, gamma_tilde):
+    return p_2 / rho_2 * (gamma_tilde / (gamma_tilde - 1))
+
+
+cp_air_200K = 1.0025 # specific heat of air at 200 Kelvin in kJ / (kg K)
+R_air = R_universal / molar_mass_air
+
+# Use the conservation of energy h + KE = constant
+h_1 = cp_air_200K * T_1
+total_h = h_1 + kinetic_1  # Total specific enthalpy
+
+epsilon_c_est = epsilon_b
+# print(f"Epsilon estimate across normal shock: {epsilon_c_est}")
+
+# Outer loop function that should coverge to 0 when we have the correct density ratio
+def outer_loop_curve_fit(epsilon_c):
+    # Given the estimated density ratio, compute the estimated density downstream of the shock
+    rho_2_c_est = rho_1 / epsilon_c
+
+    # Compute the estimated pressure downstream of the shock
+    p_2_c_est = p_1 + rho_1 * u_1 ** 2 * (1 - epsilon_c)
+
+    # Compute the estimated enthalpy downstream of the shock
+    h_2_c_est = h_1 + u_1 ** 2 / 2 * (1 - epsilon_c ** 2)
+
+    # Perform the curve fits on gamma tilde
+    gamma_tilde_est = gamma_curve.fit(p_2_c_est, rho_2_c_est)
+
+    # Compute the enthalpy from the curve fit
+    h_2_c_check = compute_curve_fit_enthaply(p_2_c_est, rho_2_c_est, gamma_tilde_est[0])
+
+    return h_2_c_check - h_2_c_est
+
+epsilon_c = sp.optimize.newton(outer_loop_curve_fit, epsilon_c_est)
+# print(f"Epsilon across normal shock: {epsilon_c}")
+
+p_2_c = p_1 + rho_1 * u_1 ** 2 * (1 - epsilon_c)
+h_2_c = h_1 + u_1 ** 2 / 2 * (1 - epsilon_c ** 2)
+rho_2_c = rho_1 / epsilon_c
+# print(f"Pressure downstream of normal shock {p_2_c}")
+# print(f"Enthalpy downstream of normal shock {h_2_c}")
+# print(f"Density downstream of normal shock {rho_2_c}")
+
+# Use temperature curve fit to get the temperature
+
+T_2_c = temp_curve.fit(p_2_c, rho_2_c)[0]
+# print(f"Temperature downstream of normal shock {T_2_c}")
+
+pressure_ratio_c = p_2_c / p_1
+density_ratio_c = rho_2_c / rho_1
+temperature_ratio_c = T_2_c / T_1
+print(f"Pressure ratio across normal shock: {pressure_ratio_c}")
+
+print(f"Density ratio across normal shock: {density_ratio_c}")
+# print(f"Epsilon across normal shock: {epsilon_b}")
+
+print(f"Temperature ratio across normal shock: {temperature_ratio_c}")

402743493315bdd37e5922a55fbec679e9313e7d
Manan Gandhi <mgandhi@gatech.edu>
2022-07-25 23:02:59 -0400

    Add setup.py

---
+++ Project/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='AE_6355_Project',
+      version='1.0',
+      description='Awesome project',
+      author='Manan Gandhi',
+      author_email='mgandhi635@gmail.com',
+      packages=['pandas',
+                'numpy',
+                'scipy',
+                'matplotlib',
+                'openpyxl',
+                'control'],
+     )
